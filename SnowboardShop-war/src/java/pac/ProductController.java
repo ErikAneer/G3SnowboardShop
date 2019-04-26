db8142e3fe23ad8f916aa89fe2284e90546757ee
ErikAneer <erik.aneer@yh.nackademin.se>
2019-04-26 11:20:37 +0200

    Added first boots-list to productController Added display of boots to index.xhtml

--- SnowboardShop-war/src/java/pac/ProductController.java
+++ SnowboardShop-war/src/java/pac/ProductController.java
@@ -20,6 +20,8 @@ public class ProductController implements Serializable {
     @EJB //injecerar en ejb
     private ProductBean productBean;
     private List<Product> boards = new ArrayList();
+    private List<Product> boots = new ArrayList();
+    private List<Product> bindings = new ArrayList();
     private String message;
     private String nameSuggestions;
 
@@ -53,28 +55,70 @@ public class ProductController implements Serializable {
         this.nameSuggestions = nameSuggestions;
     }
 
+    public ProductBean getProductBean() {
+        return productBean;
+    }
+
+    public void setProductBean(ProductBean productBean) {
+        this.productBean = productBean;
+    }
+
+    public List<Product> getBoots() {
+        return boots;
+    }
+
+    public void setBoots(List<Product> boots) {
+        this.boots = boots;
+    }
+
+    public List<Product> getBindings() {
+        return bindings;
+    }
+
+    public void setBindings(List<Product> bindings) {
+        this.bindings = bindings;
+    }
+
+    
+    
     private void createBoards() {
-        /*
-        boards.add(new Product("Birds+Of+A+Feather+146+2019+Snowboard.jpg", "Custom Smalls", "Burton", "Blabla", 140, 3490));
-        boards.add(new Product("Birds+Of+A+Feather+146+2019+Snowboard.jpg", "Custom FV 156", "Burton", "Blabla", 140, 3490));
-        boards.add(new Product("Birds+Of+A+Feather+146+2019+Snowboard.jpg", "Custom 156", "Burton", "Blabla", 140, 3490));
-        boards.add(new Product("Birds+Of+A+Feather+146+2019+Snowboard.jpg", "TWC Pro 156", "Burton", "Blabla", 140, 499990));
-        boards.add(new Product("Birds+Of+A+Feather+146+2019+Snowboard.jpg", "Custom Smalls", "Burton", "Blabla", 140, 3490));
-        boards.add(new Product("Birds+Of+A+Feather+146+2019+Snowboard.jpg", "Custom Smalls", "Burton", "Blabla", 140, 3490));
-        boards.add(new Product("Birds+Of+A+Feather+146+2019+Snowboard.jpg", "Custom Smalls", "Burton", "Blabla", 140, 3490));
-        boards.add(new Product("Birds+Of+A+Feather+146+2019+Snowboard.jpg", "Custom Smalls", "Burton", "Blabla", 140, 3490));
-        boards.add(new Product("Birds+Of+A+Feather+146+2019+Snowboard.jpg", "Custom Smalls", "Burton", "Blabla", 140, 3490));
-        */
+        
         //type productType, String brand, String name, double productLength, String description, double price, double premiumPrice, String imagePath
-        boards.add(new Product("snowboard", "Burton", "Custom Smalls", 140, "Best grom board out there!", 3490, 3490,  "/resources/images/Chicklet1152019Snowboardresized.jpg"));
-        boards.add(new Product("snowboard", "Burton", "Custom Smalls", 140, "Best grom board out there!", 3490, 3490,  "/resources/images/Custom+Smalls+140+2019+Snowboard.jpg"));
-        boards.add(new Product("snowboard", "Burton", "Custom Smalls", 140, "Best grom board out there!", 3490, 3490,  "/resources/images/Custom+Smalls+140+2019+Snowboard.jpg"));
-        boards.add(new Product("snowboard", "Burton", "Custom Smalls", 140, "Best grom board out there!", 3490, 3490,  "/resources/images/Custom+Smalls+140+2019+Snowboard.jpg"));
-        boards.add(new Product("snowboard", "Burton", "Custom Smalls", 140, "Best grom board out there!", 3490, 3490,  "/resources/images/Custom+Smalls+140+2019+Snowboard.jpg"));
+        boards.add(new Product("snowboard", "Burton", "Chicklet 120", 120, "Best grom board out there!", 1890, 1890,  "/resources/images/Chicklet1152019Snowboardresized.jpg"));
+        boards.add(new Product("snowboard", "Burton", "Custom Smalls", 140, "Best grom board out there!", 3490, 3490,  "/resources/images/CustomSmalls1402019Snowboardresized.jpg"));
+        boards.add(new Product("snowboard", "Burton", "Process 162", 162, "Best grom board out there!", 4490, 3490,  "/resources/images/Process1622019Snowboardresized.jpg"));
+        boards.add(new Product("snowboard", "Burton", "Air Retro 156", 156, "Best grom board out there!", 3490, 3490,  "/resources/images/SpeedDateRetro1562019Snowboardresized.jpg"));
+        boards.add(new Product("snowboard", "Burton", "TWC Pro 156", 156, "Best grom board out there!", 499990, 499990,  "/resources/images/BurtonTWCPro.jpg"));
+        boards.add(new Product("snowboard", "Ride", "Warpig S", 148, "Best grom board out there!", 4590, 4590,  "/resources/images/Warpig1482019Snowboardresized.jpg"));
+       
+        /*
+        boards.add(new Product("snowboard", "Ride", "Warpig S", 148, "Best grom board out there!", 4590, 4590,  "/resources/images/Warpig1482019Snowboardresized.jpg"));
+        boards.add(new Product("snowboard", "Ride", "Warpig S", 148, "Best grom board out there!", 4590, 4590,  "/resources/images/Warpig1482019Snowboardresized.jpg"));
+        boards.add(new Product("snowboard", "Ride", "Warpig S", 148, "Best grom board out there!", 4590, 4590,  "/resources/images/Warpig1482019Snowboardresized.jpg"));
+        boards.add(new Product("snowboard", "Ride", "Warpig S", 148, "Best grom board out there!", 4590, 4590,  "/resources/images/Warpig1482019Snowboardresized.jpg"));
+        boards.add(new Product("snowboard", "Ride", "Warpig S", 148, "Best grom board out there!", 4590, 4590,  "/resources/images/Warpig1482019Snowboardresized.jpg"));
+        boards.add(new Product("snowboard", "Ride", "Warpig S", 148, "Best grom board out there!", 4590, 4590,  "/resources/images/Warpig1482019Snowboardresized.jpg"));
+        boards.add(new Product("snowboard", "Ride", "Warpig S", 148, "Best grom board out there!", 4590, 4590,  "/resources/images/Warpig1482019Snowboardresized.jpg"));
+        */
+    }
+    
+    private void createBoots() {
+        
+        boots.add(new Product("boot", "32", "Lashed Melancon", 42, "Description", 3490, 3490, "/resources/images/32LashedMelancon2019Snowboardboots.jpg"));
+        boots.add(new Product("boot", "32", "Jones MTB", 42, "Description", 3490, 3490, "/resources/images/32JonesMTB2019Snowboardboots.jpg"));
+        boots.add(new Product("boot", "32", "X BT Light", 42, "Description", 3490, 3490, "/resources/images/32XBlueTomatoLight2019Snowboardboots.jpg"));
+        boots.add(new Product("boot", "Burton", "Jones MTB", 42, "Description", 3490, 3490, "/resources/images/32JonesMTB2019Snowboardboots.jpg"));
+        boots.add(new Product("boot", "Burton", "Jones MTB", 42, "Description", 3490, 3490, "/resources/images/32JonesMTB2019Snowboardboots.jpg"));
+        boots.add(new Product("boot", "Burton", "Jones MTB", 42, "Description", 3490, 3490, "/resources/images/32JonesMTB2019Snowboardboots.jpg"));
+        boots.add(new Product("boot", "Burton", "Jones MTB", 42, "Description", 3490, 3490, "/resources/images/32JonesMTB2019Snowboardboots.jpg"));
+        boots.add(new Product("boot", "Burton", "Jones MTB", 42, "Description", 3490, 3490, "/resources/images/32JonesMTB2019Snowboardboots.jpg"));
+        boots.add(new Product("boot", "Burton", "Jones MTB", 42, "Description", 3490, 3490, "/resources/images/32JonesMTB2019Snowboardboots.jpg"));
+    
     }
 
     public void onload() {
         createBoards();
+         createBoots();
     }
 
     /**
@@ -89,8 +133,8 @@ public class ProductController implements Serializable {
         List<String> matches = new ArrayList<>();
         //using data factory for getting suggestions
         for (Product p : boards) {
-            if (p.getName().toLowerCase().startsWith(enteredValue.toLowerCase())) {
-                matches.add(p.getName());
+            if ((p.getBrand().toLowerCase() + " " + p.getName().toLowerCase()).contains(enteredValue.toLowerCase())) {
+                matches.add(p.getBrand()+ " " + p.getName());
             }
         }
         return matches;
